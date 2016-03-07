80980c41c5ae504b2d522eb39d317da07111399c
May Chen <mchen@caltech.edu>
2016-03-06 18:34:13 -0800

    may made changes

--- player.cpp
+++ player.cpp
@@ -8,18 +8,17 @@
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
-
-    /* 
-     * TODO: Do any initialization you need to do here (setting up the board,
-     * precalculating things, etc.) However, remember that you will only have
-     * 30 seconds.
-     */
+	Board *current = new Board();
+	Side mySide = side;
+	Move *my-> new Move();
 }
 
 /*
  * Destructor for the player.
  */
-Player::~Player() {
+Player::~Player() 
+{
+	delete &current;
 }
 
 /*
@@ -34,10 +33,10 @@ Player::~Player() {
  * The move returned must be legal; if there are no valid moves for your side,
  * return NULL.
  */
-Move *Player::doMove(Move *opponentsMove, int msLeft) {
-    /* 
-     * TODO: Implement how moves your AI should play here. You should first
-     * process the opponent's opponents move before calculating your own move
-     */ 
-    return NULL;
+Move *Player::doMove(Move *opponentsMove, int msLeft) 
+{
+	std::cerr << "hi" << std::endl;
+	current -> doMove(opponentsMove, 
+	
+	return NULL;
 }
--- player.h
+++ player.h
@@ -11,7 +11,8 @@ class Player {
 public:
     Player(Side side);
     ~Player();
-    
+    Board *current;
+    Side mySide;
     Move *doMove(Move *opponentsMove, int msLeft);
 
     // Flag to tell if the player is running within the test_minimax context
