020c3726f25954394da10df67bcbfe46fd43fc65
May Chen <mchen@caltech.edu>
2016-03-08 00:14:10 -0800

    this is the bomb

--- player.cpp
+++ player.cpp
@@ -94,15 +94,10 @@ Move *Player::doMove(Move *opponentsMove, int msLeft)
 		{
 			score *= -23;
 		}
-		else if ((x == 2 && y == 2) || (x == 5 && y == 2) || \
-		(x == 2 && y == 5) || (x == 5 && y == 5))
-		{
-			score *= 13;
-		}
 		// Good edge cases
 		else if ((x == 0) || (x == 7) || (y == 0) || (y == 7))
 		{
-			score *= 29;
+			score *= 40;
 		}
 		// Really bad corner giving moves
 		else if ((x == 1 && y == 1) || (x == 6 && y == 6) || \
